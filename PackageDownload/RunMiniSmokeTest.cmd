399dcdecc536cfd7bb14cd116bb3c9c6b2caec1e
Thilo Steinert <thilo.steinert.external@trumpf.com>
2018-11-05 10:08:46 +0100

    sample test calls and settings added

---
+++ PackageDownload/RunMiniSmokeTest.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" HomeZone.SmokeTests\HomeZone.SmokeTests.dll /TestCaseFilter:"Name=MiniSmokeTest" /Settings:Test.runsettings
+Pause
