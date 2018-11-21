4ae060d7ca08ca40b2bdcfafc0042c02ec11ea82
Thilo Steinert <thilo.steinert.external@trumpf.com>
2018-11-21 16:51:37 +0100

    nuget.config corrected, TcReadOnlyText remark added

--- main/source/UiObjects/ControlObjects/TcReadOnlyText.cs
+++ main/source/UiObjects/ControlObjects/TcReadOnlyText.cs
@@ -13,6 +13,12 @@ namespace HomeZone.UiObjects.ControlObjects
         /// <summary>
         /// Gets the text.
         /// </summary>
+        /// <remarks>
+        /// It's necessary to use GetWpfControlText() in some cases. TcReadOnlyText mainly wraps a TextBlock,
+        /// but a TextBlock may have a child Run element that actually contains the text in which case TextBlock.Text is empty.
+        /// By using GetWpfControlText() it doesn't matter if it's directly the TextBlock or a child Run element that contains the text.
+        /// The method will return the "correct" thing as far as I could see.
+        /// </remarks>
         /// <value>
         /// The text.
         /// </value>
