1d2fd440a5c32bdfd0705d9101aeb7d488d8e83e
markus.gartmann <markus.gartmann@mcs.ch>
2019-02-06 16:48:48 +0100

    add missin usings

--- main/source/SmokeTests/FunctionalTests/TcDa07_Nesting.cs
+++ main/source/SmokeTests/FunctionalTests/TcDa07_Nesting.cs
@@ -1,5 +1,7 @@
 using HomeZone.UiCommonFunctions.Base;
 using HomeZone.UiTests.FunctionalTests.DA07_Nesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trumpf.AutoTest.Facts;
 
 namespace HomeZone.SmokeTests.FunctionalTests
 {
