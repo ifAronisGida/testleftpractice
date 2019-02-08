1f20193e860c2ed131cf056d0303baaae4722f64
Markus Gartmann <markus.gartmann@mcs.ch>
2019-02-08 16:00:45 +0100

    Create Log File for Flux Test Assembly

---
+++ main/source/FluxTests/Logging/TcFluxTestsLogging.cs
@@ -0,0 +1,18 @@
+﻿using HomeZone.UiCommonFunctions.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartBear.TestLeft;
+using System.Reflection;
+
+namespace HomeZone.FluxTests.Logging
+{
+    [TestClass]
+    class TcFluxTestsLogging : TcBaseTestClass
+    {
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            string testResultPath = GetLoggingFolder(AssemblyName.GetAssemblyName( Assembly.GetExecutingAssembly().ManifestModule.Name ).Name);
+            Driver.Log.Save( testResultPath, Log.Format.Html );
+        }
+    }
+}
