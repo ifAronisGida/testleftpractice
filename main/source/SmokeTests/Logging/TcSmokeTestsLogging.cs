9a9bfe3e1290ba6f8dd9748680542817acb6554e
Markus Gartmann <markus.gartmann@mcs.ch>
2019-02-08 16:40:02 +0100

    add logging for smoke tests

---
+++ main/source/SmokeTests/Logging/TcSmokeTestsLogging.cs
@@ -0,0 +1,21 @@
+﻿using HomeZone.UiCommonFunctions.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartBear.TestLeft;
+using System.Reflection;
+
+namespace HomeZone.SmokeTests.Logging
+{
+    [TestClass]
+    class TcSmokeTestsLogging : TcBaseTestClass
+    {
+        /// <summary>
+        /// Save Log File after finishing all tests in Assembly
+        /// </summary>
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            string testResultPath = GetLoggingFolder(AssemblyName.GetAssemblyName( Assembly.GetExecutingAssembly().ManifestModule.Name ).Name);
+            Driver.Log.Save( testResultPath, Log.Format.Html );
+        }
+    }
+}
