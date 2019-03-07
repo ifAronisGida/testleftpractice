ab57bdc7aa9cb64bbd784370fe204270fbe837b7
Peter Kovacs <p.kovacs@mortoff.hu>
2019-03-07 14:21:40 +0100

    Remove irrelevant default values from page object settings.

--- main/source/UiObjects/TcPageObjectSettings.cs
+++ main/source/UiObjects/TcPageObjectSettings.cs
@@ -12,16 +12,6 @@ namespace HomeZone.UiObjects
 
         public TimeSpan MachineFirstImportTimeout { get; set; } = TimeSpan.FromSeconds( 120 );
 
-        public TimeSpan DesignStartTimeout { get; set; } = TimeSpan.FromSeconds( 80 );
-
-        public TimeSpan CutStartTimeout { get; set; } = TimeSpan.FromSeconds( 40 );
-
-        public TimeSpan FluxStartTimeout { get; set; } = TimeSpan.FromSeconds( 15 );
-
-        public TimeSpan FluxBoostAndStartTimeout { get; set; } = TimeSpan.FromSeconds( 60 );
-
-        public TimeSpan FluxSyncTimeout { get; set; } = TimeSpan.FromSeconds( 30 );
-
         public TimeSpan SavingTimeout { get; set; } = TimeSpan.FromSeconds( 60 );
 
         public TimeSpan MaterialOverlayAppearTimeout { get; set; } = TimeSpan.FromSeconds( 20 );
